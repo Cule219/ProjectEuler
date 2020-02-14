497321332358013aa9b5b8c77f5104cbb208b102
Cule219 <culaficstefan@gmail.com>
2020-02-14 00:26:15 -0500

    7

---
+++ 7-10001-prime.js
@@ -0,0 +1,19 @@
+/**
+    By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+    What is the 10 001st prime number?
+ */
+
+let n = 15;
+let primes = [2,3,5,7,11,13];
+while(primes.length < 10001) {
+  n+=2;
+  if(isPrime(n)) {
+    primes.push(n);
+  }
+}
+console.log(n, primes[10000]);
+function isPrime(n) {
+    for(i = 2; i <= Number(Math.sqrt(n)); i++)
+        if(n % i === 0) return false;
+    return true;
+}  
