bbfbaa01bb6b47f26825ab9ce597d52cf611d148
Cule219 <culaficstefan@gmail.com>
2020-03-26 14:27:46 -0400

    20-not-done

---
+++ 20-factorial-digit-sum.js
@@ -0,0 +1,34 @@
+/**
+
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+
+ */
+
+function bigIntMultiplier(string, int) {
+  string = string + "";
+  let result = "";
+  let numbers = [];
+  for (let i = 0; i < string.length; i++) {
+    numbers.push(+string[i] * int);
+  }
+  numbers.reverse().forEach((num, i) => {
+    let existingNo = result[i]
+      ? Number(result.slice(-i - (num + "").length, -i)) + num
+      : num;
+    result = existingNo + result.slice(-i);
+  });
+  return result;
+}
+
+console.log(
+  Array.from({ length: 100 }, (_, i) => i + 1)
+    .reduce((a, v) => (a = bigIntMultiplier(a, v)))
+    .split("")
+    .reduce((a, v) => Number(v) + a, 0)
+);
+// 545
